5f47b394b526107e5cda6ad2a25a0dcaf75bad22
Renzo <ozner.dapmo@gmail.com>
2022-09-04 17:53:42 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+
+function reveal() {
+    var reveals = document.querySelectorAll(".div-anim");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+//carousel images
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// const clip = document.querySelectorAll('.clip');
+// for (let i = 0; i < clip.length; i++) {
+//     clip[i].addEventListener('mouseenter', function(e) {
+//         clip[i].play()
+//     })
+//     clip[i].addEventListener('mouseout', function(e) {
+//         clip[i].pause()
+//     })
+// }
+// document.getElementById("clip").volume = 0.1;
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
